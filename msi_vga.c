ef8fe6230cdb71cb252e72b6ab75bac50c4397c0
meeces2911 <462278+meeces2911@users.noreply.github.com>
2020-12-19 20:04:06 +1300

    Minor changes to make this work with my gt72-2qe

--- msi_vga.c
+++ msi_vga.c
@@ -5,22 +5,32 @@
 #include <stdio.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <sys/reboot.h>
 
 char *pname;
+
+//const unsigned char NVRAM_OFFSET = 0x49;	// GT73VR with bios E17A1IMS.10A
+//const unsigned char NVRAM_OFFSET = 0x4c;	// GT7x with bios 11D and(Or?) 122
+const unsigned char NVRAM_OFFSET = 0x4d;	// GT72-2QE with bios E1781IMS.110
 
 void
 print_usage ()
 {
   printf ("MSI VGA Select V0.2 by Zibri\n");
   printf ("http://www.zibri.org\n\n");
-  printf ("Usage: %s%s -s/-i/-n/-t\n",geteuid() != 0 ? "sudo ":"",pname);
+  printf ("Usage: %s%s -s -i -n -t -r\n",geteuid() != 0 ? "sudo ":"",pname);
+  printf ("-s  Gets the name of the current VGA mode\n");
+  printf ("-t  Toggles between the Nvidia and Intel VGA modes\n");
+  printf ("-n  Sets the mode to Nvidia\n");
+  printf ("-i  Sets the mode to Intel\n");
+  printf ("-r  Automatically reboots the machine\n");
   exit (EXIT_FAILURE);
 }
 
 unsigned char getvga() 
 {
     int fd1=-1;
-    unsigned char data=0x4c;      // change to 0x49 if you have MSI GT73VR with bios E17A1IMS.10A
+    unsigned char data=NVRAM_OFFSET;
     unsigned char data2=0xff;
 
     fd1=open("/dev/port",O_RDWR|O_NDELAY);
@@ -39,7 +49,7 @@ unsigned char getvga()
 void setvga(unsigned char data2) 
 {
     int fd1=-1;
-    unsigned char data=0x4c;      // change to 0x49 if you have MSI GT73VR with bios E17A1IMS.10A
+    unsigned char data=NVRAM_OFFSET;
 
     fd1=open("/dev/port",O_RDWR|O_NDELAY);
     if ( fd1 < 0 ) print_usage ();;
@@ -51,35 +61,74 @@ void setvga(unsigned char data2)
     write(fd1,&data2,1);
 
 }
+
+int set_mode(unsigned char new_mode)
+{
+
+  if (new_mode < 0x00 || new_mode > 0x01)
+  {
+    printf("Invalid/Unknown Mode selected. Aborting\n");
+    exit(EXIT_FAILURE);
+  }
+
+  char* new_mode_str = (new_mode == 0x00) ? "Intel" : "Nvidia";
+  unsigned char current = getvga();
+
+  if (current == new_mode) {
+    printf("Already running %s, nothing to do\n", new_mode_str);
+    exit(EXIT_SUCCESS);
+  }
+
+  setvga(new_mode);
+
+  if (getvga() != new_mode) {
+    printf("Unable to change to %s VGA\n", new_mode_str);
+    exit(EXIT_FAILURE);
+  }
+
+  printf("Successfully switched to: %s\n", new_mode_str);
+  printf("Reboot required\n");
+  return EXIT_SUCCESS;
+}
+
 int main(int argc, char *argv[])
 {
   int option = 0;
   pname=argv[0];
-  while ((option = getopt (argc, argv, "sint")) != -1)
+
+  unsigned char new_mode = 0x00;
+
+  while ((option = getopt (argc, argv, "sintr")) != -1)
     {
       switch (option)
 	{
 	case 's':
+	  /* Check/print what the current mode is */
           printf("%s\n",getvga() == 0x00 ? "Intel" : "Nvidia");
 	  break;
 	case 'i':
-          setvga(0x00);
-          printf("%s\n",getvga() == 0x00 ? "Intel" : "Nvidia");
+	  /* Set mode to Intel */
+	  set_mode(0x00);
 	  break;
 	case 'n':
-          setvga(0x01);
-          printf("%s\n",getvga() == 0x00 ? "Intel" : "Nvidia");
+	  /* Set mode to Nvidia */
+	  set_mode(0x01);
 	  break;
 	case 't':
-          setvga(getvga() ^ 0x01);
-          printf("%s\n",getvga() == 0x00 ? "Intel" : "Nvidia");
+	  /* Switch between Intel/Nvidia */
+	  set_mode(getvga() ^ 0x01);
 	  break;
+	case 'r':
+	  /* Reboot */
+	  sync();
+	  reboot(RB_AUTOBOOT);
+    break;
 	default:
 	  print_usage (argv[0]);
 	}
     }
 
   if (optind==1) print_usage (argv[0]);
-exit (EXIT_SUCCESS);
+  exit (EXIT_SUCCESS);
 }
 
